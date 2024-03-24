8416e12bf755898925b20bc138eb8164098dcf8a
Urvesh109 <urveshi.t@gmail.com>
2024-03-24 12:32:13 +0530

    Add simple usage for tokio::AsyncWriteExt

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use tokio::fs::File;
-use tokio::io::{self, AsyncReadExt};
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
+    let mut f = File::create("foo.txt").await?;
+
+    f.write_all(b"I/O in Tokio operates in much the same way as in std, but asynchronously.")
+        .await?;
+
+    println!("Wrotes");
+
     let mut f = File::open("foo.txt").await?;
 
     let mut buff = Vec::new();
